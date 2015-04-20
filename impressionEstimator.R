ff03cca5f3c8f725a96efc210a376e04c812cfb0
MasonGallo <masonagallo@gmail.com>
2015-04-20 17:20:35 -0400

    forgot to load the package after installing

--- impressionEstimator.R
+++ impressionEstimator.R
@@ -1,9 +1,10 @@
 # calls ui and server from github
 # use this to run the tool from any computer
 
-# if shiny isn't installed, install it
+# if shiny isn't installed, install it and load it
 if('shiny' %in% rownames(installed.packages()) == F){
-  install.packages('shiny')
+  install.packages('shiny', dependencies=T)
 }
+library(shiny)
 # runs the app from github
 runGitHub('impressionEstimator', 'danyanhavas')
