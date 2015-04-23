08f8985e61686f1d9d222586baf8a1359f732c68
MasonGallo <masonagallo@gmail.com>
2015-04-23 16:02:11 -0400

    fixed examples and edited typos

--- ui.R
+++ ui.R
@@ -1,27 +1,30 @@
 # impressionEstimator
-# 4/17/2015
+# 4/23/2015
 
 # MG comments:
-# remember user friendliness most important
-# adjusted titles and naming for some fields
-# let's change the selection of lift threshold and confidence to buttons
-# buttons should be easier to use from UX perspective
+# adjusted some text to make clear
+# fixed the example text
+# change RR to have min and max
+
+# TODO
+# need to catch all errors into:
+# "please reach out to Data Science team"
 
 shinyUI(fluidPage(
-  titlePanel("impressionEstimator"),
+  titlePanel("Impression Estimator"),
   fluidRow(
     column(3, wellPanel(
       h4("Choose the appropriate inputs for your test:"),
-      numericInput("avgRR", label = "Average Response Rate", value = 0.05),
-      helpText("Note: give the known value of your control group, 
-               e.g.current coversion rate, CTR"
-               #               "Example: if your control has 5% CTR, enter 0.05"
+      numericInput("avgRR", label = "Average Response Rate", value = 0.05,
+                   min = 0, max = 1, step=.0001),
+      helpText("Enter the known RR of your control as a decimal. 
+               Ex: if your control has a 5% CTR, enter 0.05"
       ),
       hr(),
       numericInput("num", label = "Number of Groups", value = 2),
-      helpText("Note: the total number of groups in your test, 
-               including the control group" 
-               #     "Example: if you are testing 2 alternative and 1 control, enter 3"
+      helpText("Enter the total number of groups in your test, 
+               including the control group. Ex: if you're testing
+               1 control vs 2 alternatives, enter 3"
       ),
       hr()
     )),
@@ -30,8 +33,9 @@ shinyUI(fluidPage(
       radioButtons("lift", label = "Lift Threshold",
                    choices = list("1%","5%","10%","15%","20%"),
                    selected ="10%",inline=T),
-      helpText("Note: the smallest effect you want to detect in your test"
-               #        "Example: choose 10% if you want to detect a 10% precision in CTR"
+      helpText("Select the smallest lift we will be able to 
+               detect as significant. Higher lift thresholds
+               require less impressions."
       ),
       hr()
     )),
@@ -40,8 +44,8 @@ shinyUI(fluidPage(
       radioButtons("sif", label = "Confidence Level",
                    choices = list("80%","85%","90%","95%"),
                    selected = "90%",inline=T),
-      helpText("Note: how much confidence you have for your test"
-               #     "Example: select 90% for 90% confidence"
+      helpText("Select the desired confidence level. Higher confidence
+               levels require more impressions."
       ),
       hr()
     )),
@@ -57,18 +61,17 @@ shinyUI(fluidPage(
         ),
         tabPanel("Example",
                  br(),
-                 helpText("For example, to compute the sample size needed to 
-                          pickup a 10% increase in click_through rate 
-                          at a 95% confidence level and power of test 80%:"),
+                 helpText("We want to test the performance of a new text ad.
+                          Our control text ad has a 5% CTR, and we want to
+                          be able to detect a lift of at least 10% at 90%
+                          confidence."),
                  br(),
-                 helpText("Click_through rate in the control group: 5%"),
-                 helpText("Click_through rate in the test group: 5%*(1+10%)"),
-                 helpText("Average Response Rate=0.05"),
-                 helpText("Number of groups=2"),
-                 helpText("Lift Threshold=10%"),
-                 helpText("Confidence Level=95%"),
+                 helpText("Enter Response Rate: 5%"),
+                 helpText("Enter number of groups: 2"),
+                 helpText("Select lift threshold of 10%"),
+                 helpText("Select confidence level of 90%"),
                  br(),
-                 helpText("The number of observations in each group is: 31234")
+                 helpText("The number of observations in each group is: 24603")
         )
       )
       
